2692a014a985c31467fa8e620ce52fe88f76145d
Carson Stone <carson@landis.com>
2022-04-10 14:41:02 -0400

    add config file for name, handle users enterring/leaving

--- client.js
+++ client.js
@@ -4,6 +4,7 @@ import { io as socketIO } from "socket.io-client";
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import chalk from "chalk";
+import { readFileSync } from "fs"
 
 const cliOptions = yargs(hideBin(process.argv))
   .usage("Usage: -m <message>")
@@ -24,11 +25,15 @@ const cliOptions = yargs(hideBin(process.argv))
   })
   .argv;
 
+const config = JSON.parse(readFileSync("./tx.config.json"))
+
 const serverUrl = "ws://127.0.0.1:3000"
 
+const name = cliOptions.name || config.name
+
 const io = socketIO(serverUrl, {
-  query: { name: cliOptions.name }}
-)
+  query: { name }
+})
 
 io.on("connect", () => {
   if (cliOptions.message) {
@@ -43,9 +48,20 @@ io.on("connect", () => {
 io.on("entered", (name) => {
   console.log(`${name} is now online`)
 })
+io.on("exited", (name) => {
+  console.log(`${name} has gone offline`)
+})
 
 io.on("post", (user, message) => {
   const timestamp = chalk.gray(new Date().toLocaleString())
 
   console.log(`${timestamp} - ${chalk.green(user)}: ${message}`)
 })
+
+// handle ctrl+c
+function exitHandler() {
+  io.disconnect()
+  process.exit();
+}
+
+process.on('SIGINT', exitHandler.bind(null));
--- index.js
+++ index.js
@@ -2,18 +2,29 @@ import express from "express";
 import http from "http";
 import { Server as SocketIOServer } from "socket.io";
 
+const users = {}
+
 const app = express();
 const server = http.createServer(app);
 const io = new SocketIOServer(server);
 
 io.on("connection", (socket) => {
   const { name } = socket.handshake.query
-  console.log(`${name} has connected`);
 
-  socket.broadcast.emit("entered", name)
+  if (!users[name]) {
+    users[name] = { id: socket.id }
+    console.log(`${name} has connected`);
+    socket.broadcast.emit("entered", name)
+  }
+
+  socket.on("disconnect", () => {
+    users[name] = null
+    io.emit("exited", name)
+  })
 
   socket.on("post", (message, cb) => {
     console.log(`${name}: "${message}"`)
+
     socket.broadcast.emit("post", name, `"${message}"`)
     cb("OK")
   })
